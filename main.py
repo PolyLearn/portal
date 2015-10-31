75828540ec6eba32420d76ff5e41fc9f9405cd21
Florent <clapieflor@eisti.eu>
2015-10-31 23:36:45 +0100

    add pages and design

--- main.py
+++ main.py
@@ -8,5 +8,17 @@ app.debug = True
 def index():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/products")
+def products():
+    return render_template('products.html')
+
+@app.route("/concept")
+def concept():
+    return render_template('concept.html')
+
 if __name__ == "__main__":
     app.run()
