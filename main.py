99c2d170c4046bf122e3f846de598390ad866cd7
Florent CLAPIÉ <clapieflor@eisti.eu>
2015-11-17 14:34:14 +0100

    Revert "Feature(sitemap)"

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 from flask import Flask, render_template
 from flask_frozen import Freezer
 from htmlmin.minify import html_minify
-from flask_sitemap import Sitemap
 import sys
 
 app = Flask(__name__)
 
 app.config.update(
     DEBUG=True,
-    FREEZER_RELATIVE_URLS=True,
-    SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS=True,
+    FREEZER_RELATIVE_URLS=True
 )
-
-ext = Sitemap(app=app)
 freezer = Freezer(app)
 
+@app.context_processor
+def utility_processor():
+    def app_url(app):
+        return u'http://{}.polylearn.co'.format(app)
+    return dict(app_url=app_url)
+
+@app.after_request
+def response_minify(response):
+    """
+    minify html response to decrease site traffic
+    """
+    if (not app.debug) and (response.content_type == u'text/html; charset=utf-8'):
+        response.set_data(
+            html_minify((response.get_data(as_text=True)))
+        )
+    return response
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -48,33 +60,8 @@ def thanks():
 def pedagogie():
     return render_template('pedagogie.html')
 
-
-
-
-@app.context_processor
-def utility_processor():
-    def app_url(app):
-        return u'http://{}.polylearn.co'.format(app)
-    return dict(app_url=app_url)
-
-@app.after_request
-def response_minify(response):
-    """
-    minify html response to decrease site traffic
-    """
-    if (not app.debug) and (response.content_type == u'text/html; charset=utf-8'):
-        response.set_data(
-            html_minify((response.get_data(as_text=True)))
-        )
-    return response
-
-
-
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "build":
         freezer.freeze()
-        with open("build/sitemap.xml", "r+") as f:
-             f.write(f.read().replace("http://localhost/", "http://polylearn.co/"))
-        f.close()
     else:
         app.run()
